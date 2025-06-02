4acf4e4007eca9a7d022b1669a332f5c55ee8552
Nirob Sarker <nirobsarker012@gmail.com>
2025-06-02 13:17:13 +0600

    added:apply-card

---
+++ src/components/HotJobs.jsx
@@ -0,0 +1,20 @@
+import React, { use } from 'react';
+import JobCard from './JobCard';
+
+const HotJobs = ({jobPromise}) => {
+    const jobs = use(jobPromise);
+    console.log(jobs);
+    return (
+        <div>
+            <h2 className='text-center text-2xl md:text-4xl lg:text-5xl'>Jobs of the day</h2>
+            <p className="text-center text-xl my-3.5">Search and connect with the right candidates faster.</p>
+           <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-3.5 container'>
+            {
+                jobs.map(job=><JobCard key={Math.random()} job={job}/>)
+            }
+           </div>
+        </div>
+    );
+};
+
+export default HotJobs;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "../components/Banner";
+import HotJobs from "../components/HotJobs";
 
-
+const jobPromise = fetch(`http://localhost:3000/jobs`).then(res=>res.json());
 const Home = () => {
   return <>
   <Banner/>
+  <HotJobs jobPromise = {jobPromise}/>
   </>
   ;
 };
